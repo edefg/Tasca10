465586f94d8615dc1ee59b6dc89f5e244c52a2ee
edefg <fgrau20732@iesjoanramis.org>
2023-10-24 12:36:11 +0200

    Ex 10 creat

---
+++ ex10.py
@@ -0,0 +1,11 @@
+def major(a):
+    if x>18:
+        print("Es major de edad")
+    elif x<18:
+        print("es menor de edad")
+    else:
+        print("Te 18 anys")
+
+#programa principal
+x= int(input("Introdueix la teva edad: "))
+major(x)
